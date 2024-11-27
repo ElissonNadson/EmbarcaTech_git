e3922086ad12cdd2c38c77780000c1402801d6ee
Wilton L. <wilton@gmail.com>
2024-11-27 15:57:21 -0800

    Modificacao 2 no ramo do Hello.

--- HelloWorld.c
+++ HelloWorld.c
@@ -6,6 +6,7 @@ int main() {
     printf("Ola mundo!");
     printf("Estamos em um novo RAMO do Git.");
     printf("Mais uma alteracao no arquivo.");
+    printf("Alterado pelo arquivo clone.");
 
     return 0;
 
